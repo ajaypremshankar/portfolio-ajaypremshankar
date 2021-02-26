dfe9e520259fc218fd6580134ed5949a924f6436
Ajay <ajay@Ajays-MacBook-Pro.local>
2021-02-26 17:32:40 +0530

    Added fetch api

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -4,7 +4,6 @@ import Link from 'gatsby-link';
 import { motion } from 'framer-motion';
 import Container from 'components/ui/Container';
 import TitleSection from 'components/ui/TitleSection';
-import renderHTML from 'react-render-html';
 
 import * as Styled from './styles';
 
