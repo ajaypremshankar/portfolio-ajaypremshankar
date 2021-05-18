5c5ff05d4c2b75c2433c58d78274584d7462287b
Ajay <ajaypremshankar@gmail.com>
2021-05-19 00:04:11 +0530

    Added release flag

--- src/pages/connect-form.tsx
+++ src/pages/connect-form.tsx
@@ -1,5 +1,6 @@
 import Layout from 'components/Layout';
 import SEO from 'components/SEO';
+import Container from 'components/ui/Container';
 import React from 'react';
 
 const FormApp: React.FC = () => {
@@ -7,7 +8,9 @@ const FormApp: React.FC = () => {
   return (
     <Layout>
       <SEO title="Connect with Ajay Prem Shankar, public form" />
-      <iframe id="myFrame" allowTransparency="true" allowfullscreen="true" allow="geolocation; microphone; camera" src="https://forms.app/form/609e9769001f18095acf30e5" frameborder="0" style={{width: "1px", minWidth:"100%", height:"1000px", border:"none"}}></iframe>
+      {console.log(process.env.CONNECT_FORM)}
+      {process.env.CONNECT_FORM ? <iframe id="myFrame" allowTransparency="true" allowfullscreen="true" allow="geolocation; microphone; camera" src="https://forms.app/form/609e9769001f18095acf30e5" frameborder="0" style={{width: "1px", minWidth:"100%", height:"1000px", border:"none"}}></iframe> : <Container section>Connect form launching soon.</Container> }
+
     </Layout>
   );
 };
