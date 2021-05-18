f215fc94d60448842df9bc9775d776beaefecc0e
Ajay <ajaypremshankar@gmail.com>
2021-05-19 00:16:53 +0530

    Added release flag

--- src/pages/connect-form.tsx
+++ src/pages/connect-form.tsx
@@ -8,8 +8,8 @@ const FormApp: React.FC = () => {
   return (
     <Layout>
       <SEO title="Connect with Ajay Prem Shankar, public form" />
-      {console.log(process.env.CONNECT_FORM)}
-      {process.env.CONNECT_FORM ? <iframe id="myFrame" allowTransparency="true" allowfullscreen="true" allow="geolocation; microphone; camera" src="https://forms.app/form/609e9769001f18095acf30e5" frameborder="0" style={{width: "1px", minWidth:"100%", height:"1000px", border:"none"}}></iframe> : <Container section>Connect form launching soon.</Container> }
+      {console.log(process.env.GATSBY_CONNECT_FORM)}
+      {process.env.GATSBY_CONNECT_FORM ? <iframe id="myFrame" allowTransparency="true" allowfullscreen="true" allow="geolocation; microphone; camera" src="https://forms.app/form/609e9769001f18095acf30e5" frameborder="0" style={{width: "1px", minWidth:"100%", height:"1000px", border:"none"}}></iframe> : <Container section>Connect form launching soon.</Container> }
 
     </Layout>
   );
