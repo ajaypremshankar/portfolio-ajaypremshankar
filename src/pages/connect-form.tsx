2b9798afef2ec481a2a18c8f10cf4c2b8f9be1bc
Ajay <ajaypremshankar@gmail.com>
2021-07-16 19:58:03 +0530

    Added back calendly.

--- src/pages/connect-form.tsx
+++ src/pages/connect-form.tsx
@@ -3,15 +3,18 @@ import SEO from 'components/SEO';
 import React from 'react';
 import { Link } from 'gatsby';
 import Button from 'components/ui/Button';
+import Container from 'components/ui/Container';
 
 const FormApp: React.FC = () => {
 
   return (
     <Layout>
       <SEO title="Connect with Ajay Prem Shankar, public form" />
+      <Container section>
       <Link to='/calendly/'>
           <Button>Book my calendar instead</Button>
       </Link>
+      </Container>
 
     </Layout>
   );
