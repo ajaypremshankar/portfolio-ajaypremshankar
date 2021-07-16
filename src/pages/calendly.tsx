ace122bbbcfd2557e22f45ca897e64183f69dcba
Ajay <ajaypremshankar@gmail.com>
2021-07-16 19:56:42 +0530

    Added back calendly.

--- src/pages/calendly.tsx
+++ src/pages/calendly.tsx
@@ -1,21 +1,14 @@
 import Layout from 'components/Layout';
 import SEO from 'components/SEO';
-import Button from 'components/ui/Button';
-import Container from 'components/ui/Container';
-import { Link } from 'gatsby';
 import React from 'react';
+import { InlineWidget } from 'react-calendly';
 
 const Calendly: React.FC = () => {
 
   return (
     <Layout>
       <SEO title="Ajay Prem Shankar's public calendar" />
-      <Container section>
-        <Link to='/connect-form/'>
-          <Button>Request a connect instead</Button>
-        </Link>
-      </Container>
-      {/**<InlineWidget url="https://calendly.com/ajaypremshankar/connect-with-ajay-prem-shankar" />*/}
+      <InlineWidget url="https://calendly.com/ajaypremshankar/connect-with-ajay-prem-shankar" />
     </Layout>
   );
 };
